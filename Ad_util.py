99ed7d664713ec6f130838e934baabc386f6e3f1
Stuart Sale <stuart.sale@gmail.com>
2015-12-09 18:21:31 +0000

    adding class to query for filename

--- Ad_util.py
+++ Ad_util.py
@@ -32,5 +32,46 @@ def sorted_dict_make(dirname="data/"):
                   map_name.split("_")[0])), "w") as f:
             json.dump(sorted_names, f)   
 
+class map_regions:
 
+    """ class for querying which map file is associated with
+        a certain position on the sky.
+    """
+
+    def __init__(self, dirname="data/"):
+
+        self.dirname = dirname
+        self.map_dicts = {}
+
+    def query(self, l, b):
+
+        # check we just have single floats for l&b
+        if isinstance(l, float) and isinstance(b, float):
+
+            # get the block
+            if l<215 and l>=30:
+                l_block = int(l/5.+0.5)*5
+            else:
+                raise ValueError("l out of range covered by map")
+
+            if b<0 and b>-5:
+                b_block = "a"
+            elif b>=0 and b<5:
+                b_block = "b"
+            else:
+                raise ValueError("b out of range covered by map")
+
+            blockname = "{0:d}{1:s}".format(l_block, b_block)
+
+            # if block's dict hasn't been loaded, load it
+            if blockname not in self.map_dicts:
+
+            	with open(path.join(self.dirname, 
+                     '{0:s}_map_sorted.json'.format(blockname))) as f:
+    	            self.map_dicts[blockname] = json.load(f) 
+
+            x = int((l%5)*12+0.5)
+            y = int((b%5)*12+0.5)
+
+            return self.map_dicts[blockname][x][y]
             
